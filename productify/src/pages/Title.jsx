28b7a592c004e2eeab82ce25d666ad53e1d05621
asiya-tabasum <asiyashaik7867@gmail.com>
2025-09-11 11:59:07 +0530

    fix: added time effect in title page

--- productify/src/pages/Title.jsx
+++ productify/src/pages/Title.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React,{useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import Typewriter from "typewriter-effect";
 import "@/pages/styles/Title.css";
 
 const Title = () => {
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+      navigate("/home")
+    },5000);
+    return ()=>clearTimeout(timer)
+  },[navigate])
+
   return (
     <div className="container">
       <div className="lottie">
